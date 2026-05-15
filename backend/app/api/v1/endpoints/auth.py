bee9a9ca61dc98a7a9c8efb53706ab4cbcf8a56b
bonsonalo <bonson2468@gmail.com>
2026-05-16 04:35:23 +0800

    feat: add refresh, logout and profile endpoint

--- backend/app/api/v1/endpoints/auth.py
+++ backend/app/api/v1/endpoints/auth.py
@@ -1,8 +1,8 @@
-from fastapi import HTTPException, APIRouter, Response
+from fastapi import HTTPException, APIRouter, Request, Response
 from app.schema.auth_schema import UserCreate, LoginInfo
-from app.api.dep import authenticate_user, db_dependency
+from app.api.dep import authenticate_user, db_dependency, user_authentication_dependency
 from app.core.logger import logger
-from app.service.auth_service import login_user, register_user
+from app.service.auth_service import login_user, logout_service, profile_service, refresh_token_service, register_user
 from starlette import status
 
 
@@ -31,3 +31,26 @@ async def login(user_info: LoginInfo, db: db_dependency, res: Response):
         logger.error(str(e))
         raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail= str(e))
     
+@router.post("/refresh")
+async def refresh_token(response: Response, db: db_dependency, request: Request):
+    try:
+        return await refresh_token_service(response, db, request)
+    except ValueError as e:
+        logger.error(str(e))
+        raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail= str(e))
+
+@router.post("/logout")
+async def logout(response: Response, db: db_dependency):
+    try:
+        return await logout_service(response, db)
+    except ValueError as e:
+        logger.error(str(e))
+        raise HTTPException(status_code= status.HTTP_400_BAD_REQUEST, detail= str(e))
+    
+@router.get("/profile")
+async def profile(current_user: user_authentication_dependency, db: db_dependency):
+    try:
+        return await profile_service(current_user, db)
+    except ValueError as e:
+        logger.error(str(e))
+        raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail= str(e))
