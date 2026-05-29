02f584157862c6bfc4190f5bf3c722520f8e2794
bonsonalo <bonson2468@gmail.com>
2026-05-30 01:05:21 +0800

    feat: add ana;ysis endpoint

---
+++ backend/app/api/v1/routes.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from app.api.v1.endpoints.auth import router as auth_router
+from app.api.v1.endpoints.instagram import router as instagram_router
+from app.api.v1.endpoints.analysis import router as analysis_router
+
+routers= APIRouter()
+
+router_list= [auth_router, instagram_router, analysis_router]
+
+for router in router_list:
+    routers.include_router(router)
